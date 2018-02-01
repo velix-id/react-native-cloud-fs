731581c4b786b4a5f0486cc61b13b33c0fad097b
csae0 <f.renner@gmx.at>
2018-02-01 10:40:12 +0100

    Update RNCloudFs.h
    Fix IOS Build fail because of double import

--- ios/RNCloudFs.h
+++ ios/RNCloudFs.h
@@ -1,6 +1,12 @@
 
-#import "RCTBridgeModule.h"
+//#import "RCTBridgeModule.h"
 
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
+#else
+  #import "RCTBridgeModule.h"
+#endif
+ 
 @interface RNCloudFs : NSObject <RCTBridgeModule>
 
 @end
