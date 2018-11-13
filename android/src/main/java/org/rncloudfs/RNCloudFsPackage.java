51572b2b68d4150c3ae9001863bb681f4c6ebae3
Balwant Singh <balwant.matharu@gmail.com>
2018-11-13 17:26:39 +1100

    Upgraded to Newer version of React Native

--- android/src/main/java/org/rncloudfs/RNCloudFsPackage.java
+++ android/src/main/java/org/rncloudfs/RNCloudFsPackage.java
@@ -16,7 +16,6 @@ public class RNCloudFsPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNCloudFsModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
